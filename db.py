e5134bb29eb0bdd587fcece106ff29e15c3c00a9
Denis Yanisev <janisevdan@gmail.com>
2021-05-27 19:23:01 +0300

    Add DB credentials

--- db.py
+++ db.py
@@ -1,5 +1,8 @@
 from pymongo import MongoClient
 
+MONGO_USER = 'vrodu'
+MONGO_PASSWORD = 'vrodu123'
+
 
 class DBClient:
     __db = None
@@ -7,6 +10,6 @@ class DBClient:
     def __new__(cls, *args, **kwargs):
         if cls.__db is None:
             DBClient.__db = MongoClient(
-                f'mongodb://localhost:27017/rentads')
+                f'mongodb://{MONGO_USER}:{MONGO_PASSWORD}@localhost:27017/')
 
         return cls.__db
